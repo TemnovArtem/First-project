3d08c1e1680c3a9cce73483dd923ff40323d74b5
TemnovArtem <temnov.artem@student.uzhnu.edu.ua>
2024-10-13 13:39:01 +0300

    Улучшеный css и загатовка для слайдера

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
 
     var controller = new ScrollMagic.Controller(); // создаем новою сцену с тригером на елемент logoshow
     var scene = new ScrollMagic.Scene({
-    triggerElement: '#logoshow  '
-    
+    triggerElement: '#logoshow  ' ,
+    triggerHook:0.7
     
     } ) 
     .setClassToggle('#logoshow' , 'final')  //Меняем id с logoshow на  клас final но в нашем случае добовляем клас
@@ -12,12 +12,25 @@ $(document).ready(function(){
     
     var changemain = new ScrollMagic.Controller();   // создаём новою сцену changemain где тригер будет елемент #changeformain 
     var scene = new ScrollMagic.Scene({
-    triggerElement: '#change'
-    
+    triggerElement: '#change' ,
+    triggerHook:0
     
     } )
     .setClassToggle('#change' , 'main')  //Меняем id с logoshow на  клас final но в нашем случае добовляем клас
     .addTo(changemain); 
+   
+
+var animtext = new ScrollMagic.Controller();
+var scene = new ScrollMagic.Scene({
+
+triggerElement: '#change' ,
+triggerHook:0
+
+
+})
+
+.setClassToggle('#buttonsjs' , 'buttons')
+.addTo(animtext);
 
 })
 
